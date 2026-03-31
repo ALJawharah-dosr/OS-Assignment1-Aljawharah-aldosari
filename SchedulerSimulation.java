c5bf3f8177e2c48a3623ee0fd45a13aaa6e8cd3c
ALJawharah-dosr <445052070@STD.PSAU.EDU.SA>
2026-03-31 15:54:32 +0300

    // Feature 1 is completeFeature 2: Implemented context switch counter

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -153,6 +153,8 @@ class Process implements Runnable {
 }
 
 public class SchedulerSimulation {
+    // Feature 2
+    static int contextSwitches = 0;
     public static void main(String[] args) {
         // ⚠️ IMPORTANT: Put your student ID here to seed the random number generator
         // This makes your output unique to you - DO NOT forget to change this!
@@ -228,7 +230,8 @@ public class SchedulerSimulation {
         while (!processQueue.isEmpty()) {
             // Get the next thread from the queue (FIFO)
             Thread currentThread = processQueue.poll(); // Dequeues the next thread
-            
+            // Feature 2
+            contextSwitches++;
             // Print the current process queue (list of process IDs in the queue)
             System.out.println(Colors.BOLD + Colors.MAGENTA + "┌─ Ready Queue " + "─".repeat(65) + Colors.RESET);
             System.out.print(Colors.MAGENTA + "│ " + Colors.RESET + Colors.BRIGHT_WHITE + "[" + Colors.RESET);
@@ -285,7 +288,8 @@ public class SchedulerSimulation {
         System.out.println(Colors.BOLD + Colors.BRIGHT_GREEN + 
                           "╚════════════════════════════════════════════════════════════════════════════════╝" + 
                           Colors.RESET + "\n");
-    }
+    //  Feature 2
+       System.out.println("Total context switches: " + contextSwitches);}
     
     // Method to add a process to the queue and map, while printing a "ready" message
     public static void addProcessToQueue(Process process, Queue<Thread> processQueue, 
